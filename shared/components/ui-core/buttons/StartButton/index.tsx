6ad6c205fdf89344da75012b0899fdaa8d7caffa
Rodrigo Curbelo <rodrigocurbelo14@gmail.com>
2020-09-12 12:50:00 -0400

    Animating start button

--- shared/components/ui-core/buttons/StartButton/index.tsx
+++ shared/components/ui-core/buttons/StartButton/index.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useMemo } from 'react'
+import { TouchableOpacity, Animated } from 'react-native'
 
 import styles from './styles'
 import ButtonText from '../../texts/ButtonText'
-import { TouchableOpacity } from 'react-native'
+import { animateTiming } from '../../../../helpers/animations'
 
 interface Props {
   children: ReactNode
@@ -10,9 +11,34 @@ interface Props {
 }
 
 export default function StartButton({ children, onPress }: Props) {
+  const buttonScaleAnimatedValue = useMemo(() => new Animated.Value(0), [])
+
+  useEffect(() => {
+    const triggerScaleAnimation = (toValue: number = 0) => {
+      animateTiming(buttonScaleAnimatedValue, toValue, 300, 0, true).start(() =>
+        triggerScaleAnimation(toValue === 0 ? 1 : 0)
+      )
+    }
+
+    triggerScaleAnimation()
+  }, [])
+
   return (
-    <TouchableOpacity onPress={onPress} style={styles.container}>
-      <ButtonText>{children}</ButtonText>
-    </TouchableOpacity>
+    <Animated.View
+      style={{
+        transform: [
+          {
+            scale: buttonScaleAnimatedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 1.02],
+            }),
+          },
+        ],
+      }}
+    >
+      <TouchableOpacity onPress={onPress} style={styles.container}>
+        <ButtonText>{children}</ButtonText>
+      </TouchableOpacity>
+    </Animated.View>
   )
 }
