27ae082bf1cb2853b69d8fff27b03f65bb6ae4c9
Rodrigo Curbelo <rodrigocurbelo14@gmail.com>
2020-09-12 12:33:57 -0400

    Animating Home

--- modules/Home/components/Home/index.tsx
+++ modules/Home/components/Home/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useMemo } from 'react'
+import { View, Animated } from 'react-native'
 
 import styles from './styles'
 import { NavigationProps } from '../../../../shared/types/Navigation'
 import { mapDispatchToProps, mapStateToProps } from '../..'
 import { RobotMenuSvg } from '../../../../shared/components/svg'
+import { animateTiming } from '../../../../shared/helpers/animations'
 import {
   LabelAndTitleHeader,
   StartButton,
@@ -17,17 +18,35 @@ type Props = OwnProps &
   ReturnType<typeof mapDispatchToProps>
 
 export default function Home({ startNewGame }: Props) {
+  const homeEntryAnimatedValue = useMemo(() => new Animated.Value(0), [])
+
+  useEffect(() => {
+    animateTiming(homeEntryAnimatedValue, 1, 300, 2300, true).start()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <LabelAndTitleHeader
-        spacingOnTop
-        label="Welcome to"
-        title="Trivia Challenge"
-      />
+      <Animated.View
+        style={{
+          opacity: homeEntryAnimatedValue,
+        }}
+      >
+        <LabelAndTitleHeader
+          spacingOnTop
+          label="Welcome to"
+          title="Trivia Challenge"
+        />
+      </Animated.View>
 
       <RobotMenuSvg />
 
-      <StartButton onPress={startNewGame}>Start</StartButton>
+      <Animated.View
+        style={{
+          opacity: homeEntryAnimatedValue,
+        }}
+      >
+        <StartButton onPress={startNewGame}>Start</StartButton>
+      </Animated.View>
     </View>
   )
 }
